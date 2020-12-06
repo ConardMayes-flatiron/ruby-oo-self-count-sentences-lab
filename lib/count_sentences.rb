0b26b1280ef3436a3422462b8201432d90fa4986
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-12-05 19:29:08 -0600

    commit tree 1072a5cdfee3244c9262d25b7fe406d6d3ad7116

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    sentences_array = []
+    sentences_array = self.split(/[\.\!\?]/)
+    sentences_array.delete("")
+    sentences_array.length
   end
 end
